df145ec1e9e47b4b6bcb090f23a3cf19499e7aa9
Xafree <emmanueleberst@hotmail.com>
2020-02-05 08:09:48 +0100

    tp2 sur DOM

---
+++ DOMjs/script.js
@@ -0,0 +1,15 @@
+function retirerAvecDisplay(){
+    document.getElementById("p").style.display = "none";
+}
+
+function AfficherAvecDisplay(){
+    document.getElementById("p").style.display = "";
+}
+
+function retirerAvecVisibility(){
+    document.getElementById("p").style.visibility = "hidden";
+}
+
+function AfficherAvecVisibility(){
+    document.getElementById("p").style.visibility = "visible";
+}
